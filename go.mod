ae7a9ee1b74a5d0d5e38c8bd108054b516693148
Wakar Seraj Khan <wakar473@gmail.com>
2023-12-21 11:14:27 +0530

    impl_of_queue

--- go.mod
+++ go.mod
@@ -9,19 +9,18 @@ require (
 	github.com/StackExchange/wmi v1.2.1 // indirect
 	github.com/deckarep/golang-set/v2 v2.1.0 // indirect
 	github.com/go-ole/go-ole v1.2.5 // indirect
-	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/holiman/uint256 v1.2.3 // indirect
+	github.com/holiman/uint256 v1.2.4 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
-	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/mod v0.12.0 // indirect
-	golang.org/x/tools v0.13.0 // indirect
+	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa // indirect
+	golang.org/x/mod v0.14.0 // indirect
+	golang.org/x/tools v0.15.0 // indirect
 )
 
 require (
-	github.com/ethereum/go-ethereum v1.13.5
+	github.com/ethereum/go-ethereum v1.13.6
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
@@ -35,11 +34,12 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/robfig/cron v1.2.0
+	github.com/streadway/amqp v1.1.0
 	github.com/ugorji/go/codec v1.2.7 // indirect
-	golang.org/x/crypto v0.14.0 // indirect
-	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/crypto v0.15.0 // indirect
+	golang.org/x/net v0.18.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,16 +6,23 @@ import (
 	"boilerplate/router"
 	"context"
 	"database/sql"
+	"encoding/json"
 	"log"
+	"sync"
 
 	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/robfig/cron"
+	"github.com/streadway/amqp"
+)
+
+var (
+	latestFetchedBlockNumber string
+	mutex                    sync.Mutex
 )
 
 func main() {
 	database.ConnectDb()
-	// router.Fetched()
-	// Initialize Ethereum client
+
 	client, err := rpc.Dial("https://rpc-alpha-testnet.saitascan.io")
 	if err != nil {
 		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
@@ -29,15 +36,49 @@ func main() {
 	}
 	defer db.Close()
 
+	// RabbitMQ connection details
+	rabbitMQURL := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(rabbitMQURL)
+	if err != nil {
+		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+
+	// Create a RabbitMQ channel
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Failed to open a channel: %v", err)
+	}
+	defer ch.Close()
+
+	// Declare a RabbitMQ queue
+	queueName := "block_queue"
+	_, err = ch.QueueDeclare(queueName, true, false, false, false, nil)
+	if err != nil {
+		log.Fatalf("Failed to declare a queue: %v", err)
+	}
+
 	// Create a cron job to fetch and store block details
 	cronJob := cron.New()
-	cronJob.AddFunc("@every 3s", func() {
+	cronJob.AddFunc("@every 1s", func() {
 		// Fetch the latest block number
 		var latestBlockNum string
 		if err := client.CallContext(context.Background(), &latestBlockNum, "eth_blockNumber"); err != nil {
 			log.Printf("Failed to retrieve latest block number: %v", err)
 			return
 		}
+
+		mutex.Lock()
+		defer mutex.Unlock()
+
+		// Check if the block is already stored in the database
+		if isBlockStored(db, latestBlockNum) || latestBlockNum == latestFetchedBlockNumber {
+			log.Printf("Block number %s already stored or fetched. Skipping.", latestBlockNum)
+			return
+		}
+
+		// Set the latest fetched block number
+		latestFetchedBlockNumber = latestBlockNum
 
 		// Retrieve block details for the current block
 		var block map[string]interface{}
@@ -55,16 +96,19 @@ func main() {
 		}
 
 		// Store block details in MySQL database
-		stmt, err := db.Prepare(`INSERT INTO block_details (block_number, parent_hash, block_hash, timestamp, transaction_count) VALUES (?, ?, ?, ?, ?)`)
-		if err != nil {
-			log.Printf("Failed to prepare SQL statement: %v", err)
-			return
-		}
-		defer stmt.Close()
-
-		_, err = stmt.Exec(blockDetails.Number, blockDetails.ParentHash, blockDetails.BlockHash, blockDetails.Timestamp, blockDetails.Transactions)
+		err := storeBlockDetails(db, blockDetails)
 		if err != nil {
 			log.Printf("Failed to insert block details into MySQL: %v", err)
+			return
+		}
+
+		// Publish block details to RabbitMQ
+		err = ch.Publish("", queueName, false, false, amqp.Publishing{
+			ContentType: "application/json",
+			Body:        serializeBlockDetails(blockDetails),
+		})
+		if err != nil {
+			log.Printf("Failed to publish block details to RabbitMQ: %v", err)
 			return
 		}
 
@@ -72,8 +116,37 @@ func main() {
 	})
 	cronJob.Start()
 	router.ClientRoutes()
-	// defer cronJob.Stop()
 
 	// Keep the main program running
 	select {}
+}
+
+func isBlockStored(db *sql.DB, blockNumber string) bool {
+	var count int
+	err := db.QueryRow("SELECT COUNT(*) FROM block_details WHERE block_number = ?", blockNumber).Scan(&count)
+	if err != nil {
+		log.Printf("Error checking if block is stored: %v", err)
+		return true // Assume block is stored to avoid duplicates on error
+	}
+	return count > 0
+}
+
+func storeBlockDetails(db *sql.DB, blockDetails *controller.BlockDetails) error {
+	stmt, err := db.Prepare(`INSERT INTO block_details (block_number, parent_hash, block_hash, timestamp, transaction_count) VALUES (?, ?, ?, ?, ?)`)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(blockDetails.Number, blockDetails.ParentHash, blockDetails.BlockHash, blockDetails.Timestamp, blockDetails.Transactions)
+	return err
+}
+
+func serializeBlockDetails(blockDetails *controller.BlockDetails) []byte {
+	data, err := json.Marshal(blockDetails)
+	if err != nil {
+		log.Printf("Failed to serialize block details: %v", err)
+		return nil
+	}
+	return data
 }
